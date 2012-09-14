add8b73a5ce6665e7c8c01663a1869d3621aded0
maisaengineering <contact@maisasolutions.com>
2012-09-14 14:50:06 +0530

    red bg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultiHerokuInstance::Application.config.secret_token = '4f2c76e6639665182ce516a86faae909c9fc2db1de596a04e3d855e49285a40ff3e320cc163ec51d3af3b1b905095c8e75c27b5b6041d5e96b2fe34b65fba39d'
